d3330782b412d310744b918a821b54b7fac2df57
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-09-17 20:00:03 -0300

    feat: add Item class

---
+++ src/main/java/com/pedrogallardo/cli/Item.java
@@ -0,0 +1,24 @@
+package com.pedrogallardo.cli;
+
+import java.util.List;
+
+public class Item {
+    private String name;
+    private List<Item> items;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}

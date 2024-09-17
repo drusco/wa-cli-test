a560259b090334ddad8c295ff1ac04df968ccb4c
Pedro Gallardo <pedrogallardo7011@gmail.com>
2024-09-17 20:00:58 -0300

    feat: add data class

---
+++ src/main/java/com/pedrogallardo/cli/Data.java
@@ -0,0 +1,15 @@
+package com.pedrogallardo.cli;
+
+import java.util.List;
+
+public class Data {
+    private List<Item> data;
+
+    public List<Item> getData() {
+        return data;
+    }
+
+    public void setData(List<Item> data) {
+        this.data = data;
+    }
+}
